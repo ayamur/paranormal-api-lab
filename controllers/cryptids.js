08d121922bb06cd0c7b57b626324ba198863360f
AyaMu <ayalamuruch@gmail.com>
2023-02-16 19:42:25 -0500

    add comment code for alternative option for update controller function

--- controllers/cryptids.js
+++ controllers/cryptids.js
@@ -31,6 +31,25 @@ const update = async (req, res) => {
   }
 }
 
+//!OR BELOW ALSO LOCATES
+/*
+const update = async (req, res) => {
+  try {
+    // We can also update at the instance level.
+    // Several fields can be updated at once with the set method,
+    // but we will need to save() the instance afterwards.
+
+    const cat = await Cat.findByPk(req.params.id)
+    cat.set(req.body)
+    await cat.save()
+
+    res.status(200).json(cat)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+*/
+
 module.exports = {
   create,
   index,

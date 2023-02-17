b54df6c9806907a29c2cfe075c5ff704159643b9
AyaMu <ayalamuruch@gmail.com>
2023-02-16 19:29:35 -0500

    route, create, and export index for cryptids

--- controllers/cryptids.js
+++ controllers/cryptids.js
@@ -3,12 +3,23 @@ const { Cryptid } = require('../models')
 const create = async (req, res) => {
   try {
     const cryptid = await Cryptid.create(req.body)
+    console.log(cryptid.toJSON())
     res.status(200).json(cryptid)
   } catch (error) {
     res.status(500).json(error)
   }
 }
 
+const index = async (req, res) => {
+  try {
+    const cryptids = await Cryptid.findAll()
+    res.status(200).json(cryptids)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
-  create
+  create,
+  index,
 }
--- routes/cryptids.js
+++ routes/cryptids.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const cryptidsCtrl = require('../controllers/cryptids.js')
 
 router.post('/', cryptidsCtrl.create)
+router.get('/', cryptidsCtrl.index)
 
 module.exports = router
